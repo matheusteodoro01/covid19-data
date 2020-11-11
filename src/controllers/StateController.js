000c6a81c9873a1c05b93e827c4efd8363254afd
Matheus Teodoro <60862196+matheusteodoro01@users.noreply.github.com>
2020-11-10 22:12:20 -0300

    state

--- src/controllers/StateController.js
+++ src/controllers/StateController.js
@@ -8,7 +8,7 @@ module.exports = {
 
         const { uf, state } = req.body;
         if (uf == undefined) {
-            return res.status(400).json({ message: 'uf invalided' })
+            return res.status(400).json({ message: 'uf invalid' })
         }
         else if (state == undefined) {
             return res.status(400).json({ message: 'state invalid' })
